bd64fcb6297eb052fd181cd548d79bfe156afdd1
Sankar Biswas <sankarb475@gmail.com>
2019-01-03 11:52:50 +0530

    Create MCQ.scala

---
+++ MCQ.scala
@@ -0,0 +1,12 @@
+//output of the below code would be??
+
+object dummy extends App{
+  def doSomething = {
+    List(Some("one"),None,Some("two"), Some("three"), None, Some(None))
+  }
+  val check = for {
+    func <- doSomething
+    value <- func.filter(x => x!=None)
+  } yield value
+  println(check)
+}
