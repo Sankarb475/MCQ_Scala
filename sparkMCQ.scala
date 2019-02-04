b97e1e1a6b2c8e4bb0132264a69b2350602d6917
Sankar Biswas <sankarb475@gmail.com>
2019-02-04 20:13:37 +0530

    Update sparkMCQ.scala

--- sparkMCQ.scala
+++ sparkMCQ.scala
@@ -1,3 +1,25 @@
 1. Catalyst Optimizer supports either rule-based or cost-based optimization.
 ==> False
---it only supports cost-based optimization
+--it only supports cost-based optimization.
+
+2. Among RDD, DataFrame and Dataset which one provides OOP interface?
+==> DataSet
+
+3. After transforming into DataFrame one cannot regenerate a domain object 
+==> True
+
+4. RDD allows Java serialization 
+==> True
+
+5. Which of the following make use of an encoder for serialization. 
+   DataFrame, RDD, Dataset
+==> Dataset
+
+6. Apache Spark is presently added in all major distribution of Hadoop.
+==> True
+
+7. Which of the following is good for low-level transformation and actions. 
+   RDD, DataFrame, Dataset
+==> RDD
+
+8. 
